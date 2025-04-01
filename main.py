421160a64a1339201765a5748826dc1625e984a3
slycooper2009 <112559776+slycooper2009@users.noreply.github.com>
2025-04-01 03:53:22 +0000

    Apr 1, 2025, 3:53 AM

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# testing
+from colorama import Fore
+#Opening screen
+player_1_intro = print(Fore.RED + "This is the Game that will bring your demise.")
+player_2_intro = print(Fore.RED + "The Game where you dance with death.")
+open_screen = print("WECLOME PLAYER TO OTRIO")
+#Grid List
+otrio_grid = [[' ', '|', ' ','|', ' '],
+              [' ', '|', ' ','|', ' '],
+              [' ', '|', ' ','|', ' '],
+              ['_', '|', '_','|', '_'],
+              [' ', '|', ' ','|', ' '],
+              [' ', '|', ' ','|', ' '],
+              [' ', '|', ' ','|', ' '],
+              ['_', '|', '_','|', '_'],
+              [' ', '|', ' ','|', ' '],
+              [' ', '|', ' ','|', ' '],
+              [' ', '|', ' ','|', ' '],]
+print(otrio_grid)
+#Player token list
+
+#Bot token list
+
+#rules
+
+#play
