1131eaa7f8e0af3a70ba2d95eee81e1499ece554
slycooper2009 <112559776+slycooper2009@users.noreply.github.com>
2025-04-06 23:28:48 +0000

    Apr 6, 2025, 11:28 PM

--- main.py
+++ main.py
@@ -12,41 +12,44 @@ print(otrio_grid)
 #Player token list
 def reset_tokens(colour):
     big_token = [colour + "0", "0", "0"]
-    mesium_token = [colour + "O", "O", "O"]
+    medium_token = [colour + "O", "O", "O"]
     small_token = [colour + "o", "o", "o"]
 
-    return big_token, mesium_token, small_token
+    return big_token, medium_token, small_token
 
 print(reset_tokens)
-# #bot token
-# def bot_reset_tokens():
-#     bot_big_token = ["0", "0", "0"]
-#     bot_mesium_token = ["O", "O", "O"]
-#     bot_small_token = ["o", "o", "o"]
 
 #rules
 def rules_call():
-    print("")
-
+    print(Fore.BLUE + "These are the rules for otrio"
+          ,Fore.GREEN + "\nYou and your opponent have three different tokens:" 
+          ,Fore.WHITE + "\n'o' , 'O' and '0'"
+          ,Fore.GREEN + "\nThe 'o' token is the smallest of the three tokens it can be overlapped by 'O' and '0' however this token is very effective to map out your strategy.\nThe 'O' token is medium sized it can overlap the 'o' token however it can be overlapped by the'0' token.\nFinally the '0' token is the biggest of the three and can overlap any token."
+          ,Fore.MAGENTA + "\nThe board looks like this:\n   1    2    3\nA ' '  ' '  ' '\nB ' '  ' '  ' '\nC ' '  ' '  ' '"
+          ,Fore.GREEN + "\nTo put something in the board you put the ")
+rules_call()
 #play
 def press_start():
-    choose_your_fight = input("Who do you wish to face:\nPLAYER 1 or PLAYER 2?")
-    if choose_your_fight == "PLAYER 1".lower:
-        print(otrio_grid)
-    elif choose_your_fight == "PLAYER 2".lower:
-        print(otrio_grid)
-        
-    else:
-        print(Fore.YELLOW + "Don't make me laugh that is not a number you utter buffoon. ")
-        print(Fore.RED + "Try again.")
-        while choose_your_fight != "PLAYER 1".lower or "PLAYER 2".lower or game_end:
+    game_end = False
+    while game_end != True:
+        print(Fore.MAGENTA + "Type what you want to do.")
+        what_u_want_to_do = input(Fore.BLUE + "(Press Play)"
+                                  ,Fore.GREEN + "\n(Rules)"
+                                  ,Fore.RED + "\n(I Give Up)")
+        if what_u_want_to_do == "(Press Play)":
             choose_your_fight = input("Who do you wish to face:\nPLAYER 1 or PLAYER 2?")
-        if choose_your_fight == "PLAYER 1".lower:
-            print(otrio_grid)
-        elif choose_your_fight == "PLAYER 2".lower:
-            print(otrio_grid)
+            if choose_your_fight == "PLAYER 1".lower:
+                print(otrio_grid)
+            elif choose_your_fight == "PLAYER 2".lower:
+                print(otrio_grid)
+            else:
+                print(Fore.YELLOW + "Don't make me laugh that is not a number you utter buffoon. ")
+        elif what_u_want_to_do == "(Rules)":
+            rules_call()
+        elif what_u_want_to_do == "(I Give Up)":
+            game_end = True
+
+#press_start()
 
 
-
-
-big_token, mesium_token, small_token = reset_tokens(Fore.BLUE)
+big_token, medium_token, small_token = reset_tokens(Fore.BLUE)
